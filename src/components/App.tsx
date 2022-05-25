a363b13870449248f31c2f518f2353647bf0c13a
Anton Sapeha <antonsapega@gmail.com>
2022-05-25 18:42:09 +0500

    init project

---
+++ src/components/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <h1 className={styles.greeting}>Hello World</h1>
+  );
+}
+
+export default App;
